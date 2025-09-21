3f331f92ad618966f7b90440f18e5e9517ead620
NandavardhanRadhakrishnan <nr3648@srmist.edu.in>
2025-09-21 14:22:31 +0530

    feat: initial commit

---
+++ src/main/java/com/cs/ClearingStreams/ClearingStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.cs.ClearingStreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearingStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearingStreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs/ClearingStreams/ClearingStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.ClearingStreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearingStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
